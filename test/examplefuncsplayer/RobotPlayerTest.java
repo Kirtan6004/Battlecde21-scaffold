eabe47c7818ae8283e84b4447abebe643640b1b1
Kirtan6004 <77852338+Kirtan6004@users.noreply.github.com>
2021-10-31 22:15:38 -0700

    Update RobotPlayerTest.java

--- test/examplefuncsplayer/RobotPlayerTest.java
+++ test/examplefuncsplayer/RobotPlayerTest.java
@@ -106,71 +106,9 @@ public class RobotPlayerTest {
 
 
 	}
-
-package Team2;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-
-import org.junit.Rule;
-import org.junit.Test;
-
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
-
-import static Team2.RobotPlayer.lastRobot;
-
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
-import battlecode.common.RobotType;
-
-
-public class RobotPlayerTest {
-	@Rule
-	public MockitoRule mockitoRule = MockitoJUnit.rule();
-
-	RobotController rc;
-	@Test
-	public void testSanity() {assertEquals(2, 1+1);}
-	@Test
-	public void testRobotCreation() throws GameActionException {
-		rc = mock(RobotController.class);
-		if (lastRobot == 3)
-		{
-			assertEquals(RobotType.POLITICIAN, rc.getType());
-		}
-		if (lastRobot == 1)
-		{
-			assertEquals(RobotType.SLANDERER, rc.getType());
-		}
-		if (lastRobot == 2)
-		{
-			assertEquals(RobotType.MUCKRAKER, rc.getType());
-		}
-	}
-	@Test
-	public void runTest() throws GameActionException {
-		rc = mock(RobotController.class);
-		if (rc.getType() == RobotType.ENLIGHTENMENT_CENTER)
-		{
-			assertEquals(RobotType.ENLIGHTENMENT_CENTER, rc.getType());
-		}
-		if (rc.getType() == RobotType.POLITICIAN)
-		{
-			assertEquals(RobotType.POLITICIAN, rc.getType());
-		}
-		if (rc.getType() == RobotType.SLANDERER)
-		{
-			assertEquals(RobotType.SLANDERER, rc.getType());
-		}
-		if (rc.getType() == RobotType.MUCKRAKER) {
-			assertEquals(RobotType.MUCKRAKER, rc.getType());
-		}
-	}
 	@Test
 //	Politician
 	public void PoliticianTest() throws GameActionException{
 
 	}
-}
 }
