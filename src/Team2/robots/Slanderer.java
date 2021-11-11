d25f575d960e86b5e980bc1c440a8491c50ea430
Deepa Hegde <hegde@pdx.edu>
2021-11-11 11:39:54 -0800

    Added abstract class and Slanderer class

---
+++ src/Team2/robots/Slanderer.java
@@ -0,0 +1,50 @@
+package Team2.robots;
+import Team2.RobotPlayer;
+import Team2.robots.AbstractRobot;
+import battlecode.common.*;
+
+public class Slanderer extends AbstractRobot{
+
+    public static void runSlanderer(RobotController rc) throws GameActionException {
+
+        RobotInfo[] enemies = rc.senseNearbyRobots(-1,rc.getTeam().opponent());
+        MapLocation location = rc.getLocation();
+        int result = WhenOpponentsAreFound(enemies, location, rc);
+
+    }
+
+    public static int WhenOpponentsAreFound(RobotInfo[] enemies, MapLocation location, RobotController rctemp) throws GameActionException
+    {
+        //rc = rctemp;
+        int dangerX = 0;
+        int dangerY = 0;
+        if (enemies.length > 0)
+        {
+            for(RobotInfo r : enemies)
+            {
+                if(r.getType() == RobotType.MUCKRAKER){
+                    //FLY YOU FOOLS!
+                    MapLocation enemyloc = r.getLocation();
+                    if(enemyloc.x > location.x)
+                        dangerX--;
+                    else
+                        dangerX++;
+
+                    if(enemyloc.y > location.y)
+                        dangerY--;
+                    else
+                        dangerY++;
+                }
+            }
+            MapLocation safety = location.translate(Integer.signum(dangerX),
+                    Integer.signum(dangerY));
+            tryMove(location.directionTo(safety), rctemp);
+            return 1;
+        }
+        else
+        {
+            tryMove(randomDirection(), rctemp);
+            return -1;
+        }
+    }
+}
