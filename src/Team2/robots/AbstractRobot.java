5e078176340e7b73b9223ee95c54a7b54d14dc21
Zhengmao <malker@qq.com>
2021-11-15 08:59:27 -0800

    creat abstract robots for test

---
+++ src/Team2/robots/AbstractRobot.java
@@ -0,0 +1,44 @@
+package Team2.robots;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+
+public abstract class AbstractRobot
+{
+
+    protected static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+
+    //Copied these functions over here to use in your classes
+
+    protected static boolean tryMove(Direction dir, RobotController rc) throws GameActionException
+    {
+
+        if (rc == null)
+        {
+            return false;
+        }
+        else
+        {
+            //System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+                return true;
+            } else return false;
+        }
+    }
+    protected static Direction randomDirection() {
+        return directions[(int) (Math.random() * directions.length)];
+    }
+}
